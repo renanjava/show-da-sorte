badaca7d17a6c27c67056453cea63ab59889e7e4
Renan <renancvr777@gmail.com>
2024-07-05 18:52:45 -0300

    fix: MainContent pt1

--- frontend/components/MainContent.tsx
+++ frontend/components/MainContent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import GalleryList from './GalleryList';
 
 const MainContent: React.FC = () => {
   return (
@@ -7,7 +8,7 @@ const MainContent: React.FC = () => {
 
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-lg-6 text-center" data-aos="fade-up" data-aos-delay="100">
+          <div className="col-lg-6 text-center">
             <h2><span>Este é o </span><span className="underlight">Rifas Charamba</span> um site<span> onde Itambé e região pode concorrer a prêmios com números da sorte.</span></h2>
             <p>Confira abaixo nossos prêmios disponíveis!</p>
             <a href="#lista-premios" className="btn-get-started">Prêmios</a>
@@ -22,6 +23,7 @@ const MainContent: React.FC = () => {
       <div className="container-fluid" data-aos="fade-up" data-aos-delay="100">
         <div className="row gy-4 justify-content-center">
           <ul id="lista-premios" className="row gy-4 justify-content-center">
+          
           </ul>
         </div>
       </div>
